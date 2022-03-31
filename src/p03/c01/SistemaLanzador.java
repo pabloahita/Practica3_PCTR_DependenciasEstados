13e758bb0f6e51a145eea74c6b99dc75c228fdef
Roberto <rls1004@alu.ubu.es>
2022-03-31 15:30:00 +0200

    Modificación de ActividadSalidaPuerta y SistemaLanzador

--- src/p03/c01/SistemaLanzador.java
+++ src/p03/c01/SistemaLanzador.java
@@ -6,18 +6,18 @@ public class SistemaLanzador {
 		IParque parque = new Parque(); // TODO
 		char letra_puerta = 'A';
 		
-		System.out.println("Â¡Parque abierto!");
+		System.out.println("¡Parque abierto!");
 		
 		for (int i = 0; i < Integer.parseInt(args[0]); i++) {
 			
 			String puerta = ""+((char) (letra_puerta++));
 			
-			// CreaciÃ³n de hilos de entrada
+			// Creación de hilos de entrada
 			ActividadEntradaPuerta entradas = new ActividadEntradaPuerta(puerta, parque);
 			new Thread (entradas).start();
 			
-			// 
-			// TODO
+			ActividadSalidaPuerta salidas = new ActividadSalidaPuerta(puerta, parque);
+			new Thread (salidas).start();
 			//
 			
 			
